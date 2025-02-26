f19ad0af0ddb5e1aa3fd7a5901b0479be2619c54
Scripty77 <cromsalcedovillamizar@gmail.com>
2025-02-26 08:58:48 -0400

    fix v2

--- src/components/Perfile.jsx
+++ src/components/Perfile.jsx
@@ -1,31 +1,29 @@
 export default function Perfile() {
-    return (
-      <div className="container">
-        <div className="info-content">
-          <img src="/src/assets/pfp.png" alt="Perfile-Photo" />
-        </div>
-        <div className="text">
-          <h2>
-            Hello, I'm Jean a Full-Stack developer,I Like Technology,Learning and
-            Computers.
-          </h2>
-          <a
-            href="https://www.linkedin.com/in/jean-carlos-contreras-485485279/"
-            target="_blank"
-          >
-            <img
-              src="/src/assets/linkedin-svgrepo-com.svg"
-              alt="LinkedIn"
-            />
-          </a>
-          <a href="https://github.com/Scripty77" target="_blank">
-            <img
-              src="/src/assets/github-color-svgrepo-com.svg"
-              alt="GitHub"
-            />
-          </a>
-        </div>
+  return (
+    <div className="container">
+      <div className="info-content">
       </div>
-    );
-  }
-  
+      <div className="text">
+        <h2>
+          Hello, I'm Jean a Full-Stack developer,I Like Technology,Learning and
+          Computers.
+        </h2>
+        <a
+          href="https://www.linkedin.com/in/jean-carlos-contreras-485485279/"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original-wordmark.svg"
+            alt="LinkedIn"
+          />
+        </a>
+        <a href="https://github.com/Scripty77" target="_blank">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg"
+            alt="GitHub"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -3,18 +3,17 @@ export default function Tecnologies(){
         <div className="tecnologies">
             <h2>Tecnologies</h2>
             <div className="tech-icons">
-            <img src="/src/assets/css-3-svgrepo-com.svg" alt="css-icon"/>
-            <img src="/src/assets/docker-svgrepo-com.svg" alt="docker-icon"/>
-            <img src="/src/assets/git-svgrepo-com.svg" alt="git-icon"/>
-            <img src="/src/assets/github-color-svgrepo-com.svg" alt="github-icon"/>
-            <img src="/src/assets/html-5-svgrepo-com.svg" alt="html-icon"/>
-            <img src="/src/assets/java-svgrepo-com.svg" alt="java-icon"/>
-            <img src="/src/assets/javascript-svgrepo-com.svg" alt="javascript-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" alt="css-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg"  alt="docker-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" alt="git-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg"  alt="html-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" alt="java-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="javascript-icon"/>
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg" alt="nest.js-icon"/>
-            <img src="/src/assets/linux-svgrepo-com.svg" alt="linux-icon"/>
-            <img src="/src/assets/node-svgrepo-com.svg" alt="node-icon"/>
-            <img src="/src/assets/react-svgrepo-com.svg" alt="react-icon"/>
-            <img src="/src/assets/typescript-svgrepo-com.svg" alt="typescript-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" alt="linux-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"alt="node-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg"  alt="react-icon"/>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" alt="typescript-icon"/>
             </div>
         </div>
     )
